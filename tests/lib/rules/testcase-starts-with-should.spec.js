0dfadbf74619e6089dfb531bc90ff6411daebc89
Julius Cebreros <juliusc@invenco.com>
2017-05-18 12:58:04 +1200

    Add new rule: starts with should

---
+++ tests/lib/rules/testcase-starts-with-should.spec.js
@@ -0,0 +1,27 @@
+const RuleTester = require( 'eslint' ).RuleTester;
+const rules = require( '../../../lib' ).rules;
+const ruleTester = new RuleTester();
+
+ruleTester.run( 'testcase-starts-with-should', rules[ 'testcase-starts-with-should' ], {
+    valid: [
+        'it( \'- should be valid test\' )',
+        'it( "-should be valid with quotes" )',
+        'it( "-     should be valid with quotes" )',
+        'it()',
+        'it( "should", function() {});',
+    ],
+    invalid: [
+        {
+            code: 'it( \' fail on no dash\' )',
+            errors: [ {
+                message: 'Test titles should start with should.'
+            }]
+        },
+        {
+            code: 'it( \'*should be valid test\' )',
+            errors: [ {
+                message: 'Test titles should start with should.'
+            }]
+        }
+    ]
+})
