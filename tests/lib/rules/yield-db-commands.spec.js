c6d859b97bc36bef795776ae5e596b885a3cb608
James Santos <icqhv.santos@gmail.com>
2017-10-02 11:08:52 +1300

    Add 'yield-db-commands' rule

---
+++ tests/lib/rules/yield-db-commands.spec.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const rule = require('../../../lib/rules/yield-db-commands')
+const RuleTester = require('eslint/lib/testers/rule-tester')
+const ruleTester = new RuleTester()
+
+function buildTest(code) {
+    return 'function* test() { ' + code + ' }'
+}
+
+ruleTester.run('yield-db-commands', rule, {
+    valid: [
+        {
+            code: buildTest(
+                `const res = yield server.db.read.row(
+                    \`SELECT * FROM us;\`
+                )`
+            )
+        },
+        {
+            code: buildTest(
+                `let reports = yield server.db.read.rows( \`
+                        SELECT id, module, name, base_url, active, description, require_roles
+                        FROM report;
+                \` );`
+            )
+        },
+        {
+            code: buildTest(`yield server.db.write.execute( 'UPDATE ics_alarm.alarm SET deleted = true WHERE id = $1', [ id ] )`)
+        },
+        {
+            code: buildTest(`const connection = yield server.db.write.getConnection();`)
+        },
+        {
+            code: buildTest(`yield conn.execute( 'COMMIT' )`)
+        }
+    ],
+    invalid: [
+        {
+            code: buildTest(
+                `const res = server.db.read.row(
+                    \`SELECT * FROM us;\`
+                )`
+            ),
+            output: buildTest(
+                `const res = yield server.db.read.row(
+                    \`SELECT * FROM us;\`
+                )`
+            ),
+            errors: [{message: 'server.db.read.row must be yielded'}]
+        },
+        {
+            code: buildTest(
+                `conn.execute( \`
+                    UPDATE target SET 
+                        site_id = $1
+                        WHERE target_id = $2;
+                \`, [ updatePayload.siteId, device.id ] )`
+            ),
+            output: buildTest(
+                `yield conn.execute( \`
+                    UPDATE target SET 
+                        site_id = $1
+                        WHERE target_id = $2;
+                \`, [ updatePayload.siteId, device.id ] )`
+            ),
+            errors: [{message: 'conn.execute must be yielded'}]
+        }
+    ]
+})
